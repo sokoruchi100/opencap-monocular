ce9462bd2749c0c81781802cf7b04606f2109c1c
sokoruchi100 <harmeyforever5435@gmail.com>
2026-03-31 19:14:21 +0000

    fix: installation process

--- third_party_modified/ipman/utils_ipman/saver.py
+++ third_party_modified/ipman/utils_ipman/saver.py
@@ -98,6 +98,6 @@ class CheckpointSaver:
                     )
         checkpoint_list = sorted(checkpoint_list)
         self.latest_checkpoint = (
-            None if (len(checkpoint_list) is 0) else checkpoint_list[-1]
+            None if (len(checkpoint_list) == 0) else checkpoint_list[-1]
         )
         return
